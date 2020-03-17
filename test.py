1c9e68dee9c38d4c46e6dd78a66560b2543f5ca4
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-18 02:00:59 +0530

    Added test script

---
+++ test.py
@@ -0,0 +1,34 @@
+import gym
+import json
+from src.utils import *
+from src.agents import *
+
+
+class Tester:
+
+    def __init__(self, config_file, model_file):
+        self.config = Tester.parse_config(config_file)
+        self.env = gym.make(self.config['env_name'])
+        self.state_dimension = self.env.observation_space.shape[0]
+        self.action_dimension = self.env.action_space.shape[0]
+        self.max_action = float(self.env.action_space.high[0])
+        self.device = torch.device('cpu')
+        self.agent = TD3Agent(
+            state_dim=self.state_dimension, action_dim=self.action_dimension,
+            max_action=self.max_action, device=self.device,
+            discount=self.config['discount'], tau=self.config['tau']
+        )
+        self.agent.load(model_file)
+
+    @staticmethod
+    def parse_config(json_file):
+        with open(json_file, 'r') as f:
+            configs = json.load(f)
+        return configs
+
+    def test(self, eval_episodes, render=True):
+        self.mean_rewards = evaluate_policy(
+            self.agent, self.config['env_name'], self.config['seed'],
+            eval_episodes=eval_episodes, render=render
+        )
+        print(self.mean_rewards)
