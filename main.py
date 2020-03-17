3ff08d7724a97e44520c5432a64db6ae1d6f7f95
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-18 02:02:58 +0530

    Added driver code

---
+++ main.py
@@ -0,0 +1,14 @@
+from test import Tester
+
+
+# tester = Tester(
+#     './Configs/BipedalWalker-v3.json',
+#     './pretrained_models/bipedal_walker_v3/bipedal_walker_v3_0'
+# )
+
+tester = Tester(
+    './Configs/LunarLanderContinuous-v2.json',
+    './pretrained_models/lunar_lander_v2/ddpg_lunarlander_v2_0'
+)
+
+print('Mean Reward:', tester.test(eval_episodes=20, render=True))
