c3431a63180bea951de92b0beb969671ba9133d2
soumik12345 <19soumik.rakshit96@gmail.com>
2020-03-19 21:12:28 +0530

    Added Configs

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from test import Tester
 #     './pretrained_models/bipedal_walker_v3/bipedal_walker_v3_0'
 # )
 
+# tester = Tester(
+#     './Configs/LunarLanderContinuous-v2.json',
+#     './pretrained_models/lunar_lander_v2/ddpg_lunarlander_v2_0'
+# )
+
 tester = Tester(
-    './Configs/LunarLanderContinuous-v2.json',
-    './pretrained_models/lunar_lander_v2/ddpg_lunarlander_v2_0'
+    './Configs/BipedalWalkerHardcore-v3.json',
+    './pretrained_models/bipedal_walker_hardcore_v3/bipedal_walker_hardcore_v3_0'
 )
 
 print('Mean Reward:', tester.test(eval_episodes=20, render=True))
